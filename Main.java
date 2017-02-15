8eceaef9554a662b9137ecb8001ef6a7549dc18c
Pedrum <pedrum.jalali@gmail.com>
2017-02-15 11:52:11 -0500

    using new packages for project

--- Main.java
+++ Main.java
@@ -1,70 +1,45 @@
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Date;
+import MySqlJava.*;
+
 
 /*reads data from the fileName file and exports them to the mySQL database. It also generates flows*/
 public class Main {
-    private static MySqlCon mySqlCon;
+    private static Queries queries;
     public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException{
-        	mySqlCon = new MySqlCon("D11", "fafdRE$3", "192.168.20.12");
+            dbParams _dbParams = new dbParams("192.168.20.12", "fafdRE$3", "D11" );
+        	queries = new Queries(_dbParams);
         	ReadData();
     }
     private static void ReadData() throws IOException{
         //make sure j is in sync with the counter for packets
         int j;
         Packet _packet;
-        int prevID;
         Date x = new Date();
-        long durationNA=0;
-        long durationMax=0;
-        long durationUpdate =0;
-        long durationUpdatetest =0;
+
         long startTime =0;
         long endTime;
-        long durationInsertFlow = 0;
         System.out.println(x.toString());
         j =0;
             try{
-
-
-             //    startTime = System.nanoTime();
-                _packet = mySqlCon.Get_packet_NA();
-              //   endTime  = System.nanoTime();
-               //  durationNA += (endTime - startTime)/1000000000;
-
+                _packet = queries.Get_packet_NA();
                 while (_packet != null){
-                   // startTime = System.nanoTime();
-                    mySqlCon.Insert_Flow(_packet);
-                   // endTime  = System.nanoTime();
-                    //durationInsertFlow += (endTime - startTime)/1000000000;
-
-
-
-
-
-
-                    //startTime = System.nanoTime();
-                    _packet = mySqlCon.Get_packet_NA();
-                    //endTime  = System.nanoTime();
-                    //durationNA += (endTime - startTime)/1000000000;
-
-                j++;
+                    queries.Insert_Flow(_packet);
+                    _packet = queries.Get_packet_NA();
+                    j++;
                     System.out.println(j);
-                   // System.out.println(j);
                     if (j % 10000 ==0){
                         System.out.println(j);
                         endTime  = System.nanoTime();
                         System.out.println((endTime - startTime)/1000000000);
                         startTime =endTime;
                     }
-
                 }
             }
             catch(java.sql.SQLException ex){
                 System.out.println(ex);
-
             }
-
       System.out.println(x.toString());
 
 
